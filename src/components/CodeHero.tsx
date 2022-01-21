dd15ef19726e177cfd8ce92e1f0d715d33c01d70
pchenr1que1 <pchenr1que1@gmail.com>
2022-01-21 18:07:15 -0300

    first page finished

---
+++ src/components/CodeHero.tsx
@@ -0,0 +1,190 @@
+import { Text } from "@chakra-ui/react";
+
+export default function CodeHero() {
+  return (
+    <Text textShadow="49px 41px 0px rgba(0, 0, 0, 0.25)" color="gray.200">
+      <code
+        style={{
+          color: "#E7CA60",
+        }}
+      >
+        import
+      </code>
+      <code>{` {`}</code>
+      <code
+        style={{
+          color: "#FE7389",
+        }}
+      >{` useState `}</code>
+      <code>{`} `}</code>
+      <code
+        style={{
+          color: "#E7CA60",
+        }}
+      >{`from `}</code>
+      <code
+        style={{
+          color: "#3BE582",
+        }}
+      >{`"react"`}</code>
+      <code>{`;`}</code>
+
+      <br />
+      <br />
+      <code
+        style={{
+          color: "#E7CA60",
+        }}
+      >
+        {`
+          export default
+          `}
+      </code>
+      <code
+        style={{
+          color: "#22E6D6",
+        }}
+      >{`function `}</code>
+      <code
+        style={{
+          color: "#64BAF3",
+        }}
+      >{`personalInfo`}</code>
+      <code>{`() {`}</code>
+
+      <br />
+      <code
+        style={{
+          marginLeft: "2.5rem",
+          color: "#22E6D6",
+        }}
+      >{`
+        const
+        `}</code>
+      <code>[</code>
+      <code
+        style={{
+          color: "#FE7389",
+        }}
+      >
+        currentRole
+      </code>
+      <code>, </code>
+      <code
+        style={{
+          color: "#64BAF3",
+        }}
+      >
+        setCurrentRole
+      </code>
+      <code>] = </code>
+      <code
+        style={{
+          color: "#64BAF3",
+        }}
+      >
+        useState
+      </code>
+      <code>(</code>
+      <code style={{ color: "#3BE582" }}>"freelancer"</code>
+      <code>);</code>
+      <br />
+      <br />
+      <code
+        style={{
+          marginLeft: "2.5rem",
+          color: "#22E6D6",
+        }}
+      >{`
+        const 
+        `}</code>
+      <code
+        style={{
+          color: "#FE7389",
+        }}
+      >
+        personalInfo
+      </code>
+      <code>{` = {`}</code>
+      <br />
+      <code
+        style={{
+          marginLeft: "5rem",
+        }}
+      >
+        {`
+          name: 
+          `}
+      </code>
+      <code
+        style={{
+          color: "#3BE582",
+        }}
+      >
+        "Pedro Correia"
+      </code>
+      <code>,</code>
+      <br />
+      <code
+        style={{
+          marginLeft: "5rem",
+        }}
+      >
+        {`
+          locale: 
+          `}
+      </code>
+      <code
+        style={{
+          color: "#3BE582",
+        }}
+      >
+        "Maceió-AL, Brazil"
+      </code>
+      <code>,</code>
+      <br />
+      <code
+        style={{
+          marginLeft: "5rem",
+        }}
+      >
+        {`
+          age: 
+          `}
+      </code>
+      <code style={{ color: "#3BE582" }}>25</code>
+      <code>,</code>
+      <br />
+      <code
+        style={{
+          marginLeft: "5rem",
+        }}
+      >
+        {`
+          occupation: 
+          `}
+      </code>
+      <code
+        style={{
+          color: "#3BE582",
+        }}
+      >
+        {`["full-stack developer", "entrepreneur", "visual artist", "photographer"]`}
+      </code>
+      <br />
+      <code
+        style={{
+          marginLeft: "2.5rem",
+        }}
+      >
+        {`
+          };
+          `}
+      </code>
+      <br />
+      {`
+          }
+        `}
+    </Text>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,10 @@ import {
   Grid,
   GridItem,
 } from "@chakra-ui/react";
+import Image from "next/image";
 import ReactCountryFlag from "react-country-flag";
+import CodeHero from "../components/CodeHero";
+import { SiTypescript, SiJavascript, SiPython } from "react-icons/si";
 
 export default function Index() {
   return (
@@ -19,9 +22,9 @@ export default function Index() {
           <HStack w="100%" align="flex-start" ml="5rem" spacing="24">
             <Text
               color="gray.200"
+              cursor="pointer"
               _hover={{
                 color: "red.500",
-                cursor: "pointer",
               }}
               fontFamily="PlayFair Display"
               fontSize="1.125rem"
@@ -30,6 +33,7 @@ export default function Index() {
             </Text>
             <Text
               color="gray.200"
+              cursor="pointer"
               _hover={{
                 color: "red.500",
                 cursor: "pointer",
@@ -41,6 +45,7 @@ export default function Index() {
             </Text>
             <Text
               color="gray.200"
+              cursor="pointer"
               _hover={{
                 color: "red.500",
                 cursor: "pointer",
@@ -52,6 +57,7 @@ export default function Index() {
             </Text>
             <Text
               color="gray.200"
+              cursor="pointer"
               _hover={{
                 color: "red.500",
                 cursor: "pointer",
@@ -63,6 +69,7 @@ export default function Index() {
             </Text>
             <Text
               color="gray.200"
+              cursor="pointer"
               _hover={{
                 color: "red.500",
                 cursor: "pointer",
@@ -74,6 +81,7 @@ export default function Index() {
             </Text>
             <Text
               color="gray.200"
+              cursor="pointer"
               _hover={{
                 color: "red.500",
                 cursor: "pointer",
@@ -116,28 +124,50 @@ export default function Index() {
         </Box>
       </Flex>
 
-      <Grid
-        zIndex="3"
-        position="absolute"
-        top="20vh"
-        left="20vw"
-        templateColumns="repeat(5, 1fr)"
-        templateRows="repeat(5, 1fr)"
-        gap={0}
-      >
-        <GridItem gridArea="grid-area: 4 / 1 / 6 / 6">
-          <Text>import</Text>
-        </GridItem>
-        <GridItem gridArea="grid-area: 1 / 1 / 4 / 4">
-          <Box bgColor="gray.200" w="100%" h="100%" />
-        </GridItem>
-        <GridItem gridArea="grid-area: 1 / 4 / 4 / 6">
-          <Box bgColor="gray.200" w="100%" h="100%" />
-        </GridItem>
-      </Grid>
+      <VStack position="absolute" zIndex="3" top="20vh" left="10vw">
+        <HStack spacing="28">
+          <Box w="760px" h="300px">
+            <CodeHero />
+          </Box>
+
+          <Box w="340px" h="330px">
+            <VStack spacing="4">
+              <Image src="/images/profile.png" height="140px" width="140px" />
+
+              <Text
+                w="75%"
+                fontSize=".8rem"
+                textJustify="inter-word"
+                textAlign="justify"
+                fontFamily="Playfair Display"
+                color="gray.200"
+              >
+                developing film since 2021, shoothing photography since 2012,
+                developing software since 2020, mentally affected* by journalism
+                college since 2014
+                <br />
+                <br />
+                *in a good way
+              </Text>
+
+              <HStack spacing="10">
+                <SiTypescript color="#AAAAAA" size="35px" />
+                <SiJavascript color="#AAAAAA" size="35px" />
+                <SiPython color="#AAAAAA" size="35px" />
+              </HStack>
+            </VStack>
+          </Box>
+        </HStack>
+        <HStack spacing="0" w="1080px" h="285px">
+          <Image src="/images/home1.png" height="285px" width="270" priority />
+          <Image src="/images/home2.png" height="285px" width="270" priority />
+          <Image src="/images/home3.png" height="285px" width="270" priority />
+          <Image src="/images/home4.png" height="285px" width="270" priority />
+        </HStack>
+      </VStack>
 
       <Center zIndex="2" position="absolute" h="100vh" w="100vw" mt="5rem">
-        <Box borderRadius="100%" h="31.25rem" w="31.25rem" bgColor="red.500" />
+        <Box borderRadius="100%" h="38rem" w="38rem" bgColor="red.500" />
       </Center>
     </Flex>
   );
