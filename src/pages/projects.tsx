8dc48c87069d02cb43a85b74afaaeb95454b4609
pchenr1que1 <pchenr1que1@gmail.com>
2022-01-22 23:38:12 -0300

    second update

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,47 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  IconButton,
+  Text,
+  VStack,
+  Link as ChakraLink,
+  HStack,
+} from "@chakra-ui/react";
+import WorkList from "../components/workList";
+import Link from "next/link";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import WorkItemTest from "../components/WorkItemTest";
 
 export default function Projects() {
-  return <Text color="gray.200">Projects</Text>;
+  return (
+    <Flex w="100vw" h="100vh" position="relative" overflow="hidden">
+      <Link href={`/`} passHref>
+        <ChakraLink style={{ textDecoration: "none" }}>
+          <Box position="absolute" left="5rem" top="2rem">
+            <IconButton
+              bgColor="transparent"
+              color="gray.200"
+              cursor="pointer"
+              _hover={{
+                bgColor: "transparent",
+                color: "red.500",
+              }}
+              aria-label="backward"
+              icon={<ArrowBackIcon h="20px" w="20px" />}
+            />
+          </Box>
+        </ChakraLink>
+      </Link>
+
+      <VStack mt="5rem" ml="6rem">
+        <HStack spacing="10">
+          <WorkItemTest />
+        </HStack>
+      </VStack>
+
+      <Flex position="absolute" right="12rem" top="8rem">
+        <Box borderRadius="100%" h="38rem" w="38rem" bgColor="red.500" />
+      </Flex>
+    </Flex>
+  );
 }
