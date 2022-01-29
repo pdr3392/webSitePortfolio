bd32a94088c6609c31c0cc87867c0b49bcd64edd
pchenr1que1 <pchenr1que1@gmail.com>
2022-01-29 08:46:26 -0300

    updating3

--- src/components/EightDec.tsx
+++ src/components/EightDec.tsx
@@ -1,6 +1,8 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
+import tiff288 from "../../public/images/eightdec/tiff288.jpg";
+import tiff280 from "../../public/images/eightdec/tiff280.jpg";
 
 import SwiperCore, {
   Navigation,
@@ -11,10 +13,14 @@ import SwiperCore, {
 
 import "swiper/css";
 import "swiper/css/pagination";
+import { useState } from "react";
+import useNextBlurhash from "use-next-blurhash";
 
 SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
 
 export default function EightDec() {
+  const [blurData] = useNextBlurhash("LjHLl1t7j[j[~qRjj[j[WBM{t7of");
+
   return (
     <Flex>
       <Box h="100%" w="100%">
@@ -35,7 +41,13 @@ export default function EightDec() {
           className="mySwiper"
         >
           <SwiperSlide>
-            <Image src="/images/eightdec/tiff280.jpg" layout="fill" priority />
+            <Image
+              placeholder="blur"
+              blurDataURL={blurData}
+              src={tiff280}
+              layout="fill"
+              priority
+            />
           </SwiperSlide>
           <SwiperSlide>
             <Image src="/images/eightdec/tiff283.jpg" layout="fill" priority />
@@ -47,7 +59,7 @@ export default function EightDec() {
             <Image src="/images/eightdec/tiff287.jpg" layout="fill" priority />
           </SwiperSlide>
           <SwiperSlide>
-            <Image src="/images/eightdec/tiff288.jpg" layout="fill" priority />
+            <Image placeholder="blur" src={tiff288} layout="fill" priority />
           </SwiperSlide>
           <SwiperSlide>
             <Image src="/images/eightdec/tiff289.jpg" layout="fill" priority />
